1e8bd722d60aa69966528d6d999bdfc96b70659a
Mohib <mohibullahmohim2020@gmail.com>
2024-08-17 13:42:05 +0600

    Search and sort function add

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 8000;
 
-// middle-wares 
+// middle-wares
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,31 +18,64 @@ const client = new MongoClient(uri, {
   },
 });
 
-
-//https://refurbished-mobile-server.vercel.app/ 
+//https://refurbished-mobile-server.vercel.app/
 async function run() {
   try {
     const database = client.db("Refurbished");
     const productsCol = database.collection("products");
 
-    // All products get
-    app.get('/products', async(req, res)=>{
-
+    // // search kore deki
+    app.get('/some-products', async(req, res)=>{
       try {
-        const result = await productsCol.find().toArray();
-        res.status(200).send({
-            success: true,
-            message: 'Products retrieved successfully',
-            data: result
-        });
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: 'Failed to retrieve products',
-            error: error.message
-        });
-    }   
+        const { productName } = req.query;
+        let query = {};
 
+        if (productName) {
+          // Using $regex for case-insensitive and partial match search
+          query.productName = { $regex: productName, $options: 'i' };
+        }
+        // Find products that match the query and sort them
+        const products =await productsCol.find(query).toArray();
+        res.json(products);
+
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ message: 'Server error' });
+      }
+    })
+
+    // All products get
+    app.get("/products", async (req, res) => {
+      try {
+        const {sort } = req.query;
+        let sortOption = {};   
+
+        if (sort === "lowhigh") {          
+          sortOption.price = 1; // Ascending order
+        } else if (sort === "highlow") {
+          sortOption.price = -1; // Descending order
+        } else if (sort === "") {
+          const result = await productsCol.find().toArray();
+          res.status(200).send({
+            success: true,
+            message: "Products retrieved successfully",
+            data: result,
+          });
+        } else {
+          sortOption = {};
+        }
+        const products = await productsCol
+          .find()
+          .sort(sortOption)
+          .toArray();
+        res.json(products);
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: "Failed to retrieve products",
+          error: error.message,
+        });
+      }
     });
 
     console.log(
